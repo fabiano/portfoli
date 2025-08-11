6bf8231ab13b9f4c0b8c9a9c2340bf6914234e59
Fabiano França <fabiano.franca@gmail.com>
2025-08-11 19:54:57 +0100

    Run the migration runner when starting the app host

--- src/Portfoli.AppHost/AppHost.cs
+++ src/Portfoli.AppHost/AppHost.cs
@@ -3,12 +3,13 @@ var database = builder.AddSqlite("database");
 
 builder
     .AddProject<Projects.Portfoli_Api>("api")
-    .WithReference(database, connectionName: "Database");
+    .WithReference(database, connectionName: "Database")
+    .WaitFor(database);
 
 builder
     .AddProject<Projects.Portfoli_Infra_MigrationRunner>("migration-runner")
     .WithReference(database, connectionName: "Database")
-    .WithExplicitStart();
+    .WaitFor(database);
 
 var app = builder.Build();
 
